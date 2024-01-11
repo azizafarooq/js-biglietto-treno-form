8c7796aa92181545a0b0c0472efff1be302ca9a4
azizafarooq <azizafarooq2000@gmail.com>
2024-01-11 14:59:41 +0100

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,54 @@
+const usernameInput = document.getElementById('user-name');
+const genderInput = document.getElementById('user-age');
+const kmInput = document.getElementById('user-km');
+
+
+
+
+
+/*const prezzoBiglietto = document.getElementById("prezzo-biglietto");
+
+const utenteKm = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+console.log(utenteKm);
+
+const utenteEtà = parseInt(prompt("Quanti anni hai?"));
+console.log(utenteEtà);
+
+const result = utenteKm * 0.21;
+console.log(result);
+
+const finalResult = result.toFixed(2);
+
+if (!isNaN(utenteEtà) && !isNaN(utenteKm)){
+    if (utenteEtà < 18) {
+        const discountPerc= 20;
+    
+        const discount = result * discountPerc / 100;
+        console.log(discount);
+    
+        const price = result - discount;
+       
+        const finalPrice =price.toFixed(2);
+        console.log(finalPrice);
+    
+        prezzoBiglietto.innerText = "Il prezzo del tuo biglietto è " + finalPrice;
+    } else if (utenteEtà >= 65) 
+    {
+        const discountPerc= 40;
+    
+        const discount = result * discountPerc / 100;
+        console.log(discount);
+    
+        const price = result - discount;
+        
+        const finalPrice =price.toFixed(2);
+        console.log(finalPrice);
+    
+        prezzoBiglietto.innerText = "Il prezzo del tuo biglietto è " + finalPrice;
+    } else {
+        prezzoBiglietto.innerText = "Il prezzo del tuo biglietto è " + finalResult;
+    }    
+} else{
+    prezzoBiglietto.innerText = "I valori inseriti non sono validi. Ricaricare la pagina.";
+}
+*/
